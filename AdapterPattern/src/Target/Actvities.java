bb52c590d6a5c85472c02a465d4f93cb619cd1ad
Saeeds28 <awesomesaad.saeed2@gmail.com>
2019-12-07 16:42:40 -0500

    created the general interface that every class must implement

---
+++ AdapterPattern/src/Target/Actvities.java
@@ -0,0 +1,15 @@
+package Target;
+
+/**
+ * List of methods that clients can call. This is the thing that will be targeted by the
+ * adapter class
+ * 
+ * @author Saad
+ *
+ */
+public interface Actvities {
+	void eat();
+	void work();
+	void kill();
+	void sleep();
+}
